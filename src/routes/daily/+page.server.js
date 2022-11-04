f2b598d2f73e01e09843ac99909ab53f7279da5d
rrirrirr <drpihl@gmail.com>
2022-11-04 13:48:44 +0100

    formatting

--- src/routes/daily/+page.server.js
+++ src/routes/daily/+page.server.js
@@ -3,28 +3,28 @@ import axios from 'axios'
 
 export async function load({ params }) {
   const res = await axios.get(`${import.meta.env.VITE_SCOREURL}/dailyseed`)
-	return {
-		seed: res.data
-	}
+  return {
+    seed: res.data
+  }
 }
 
 export const actions = {
-	score: async ({ request }) => {
-		const data = await request.formData()
-		console.log(data)
-		const name = data.get('name')
-		const score = data.get('score')
-		if (!name.length) {
-			return invalid(400, { name, missing: true })
-		}
-		if (name.toLowerCase() === 'you') {
-			return invalid(400, { name, incorrect: true })
-		}
-		try{
-  		await axios.post(`${import.meta.env.VITE_SCOREURL}/score`, { name, score: parseInt(score) })
-		} catch (error) {
-			console.log(error)
-		}
-		return { success: true }
-	}
+  score: async ({ request }) => {
+    const data = await request.formData()
+    console.log(data)
+    const name = data.get('name')
+    const score = data.get('score')
+    if (!name.length) {
+      return invalid(400, { name, missing: true })
+    }
+    if (name.toLowerCase() === 'you') {
+      return invalid(400, { name, incorrect: true })
+    }
+    try {
+      await axios.post(`${import.meta.env.VITE_SCOREURL}/score`, { name, score: parseInt(score) })
+    } catch (error) {
+      console.log(error)
+    }
+    return { success: true }
+  }
 }
