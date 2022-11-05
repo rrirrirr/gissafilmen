1f079a6344b9a74b2d165e59973936aebbbfdfce
rrirrirr <drpihl@gmail.com>
2022-11-05 15:33:57 +0100

    add ratingtop

--- src/lib/gameChooser.js
+++ src/lib/gameChooser.js
@@ -1,10 +1,13 @@
 import { yearGame } from './games/year'
 import { ratingGame } from './games/rating'
+import { ratingTopGame } from './games/ratingtop'
 
 const games = [
   yearGame,
-  ratingGame
+  ratingGame,
+  ratingTopGame
 ]
+
 export default function gameChooser() {
 	const index  =  Math.floor(Math.random() * games.length)
 	return games[index]
